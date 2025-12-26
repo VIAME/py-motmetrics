1ce233da96adc9eeeae5ca993eac0be068690ed4
Matt Dawkins <matt.dawkins@kitware.com>
2025-12-25 19:30:03 -0500

    Change pathing for files

--- setup.py
+++ setup.py
@@ -11,15 +11,18 @@ try:
 except ImportError:
     from distutils.core import setup
 
-with io.open("requirements.txt") as f:
+# Get the directory where setup.py is located
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, "requirements.txt")) as f:
     required = f.read().splitlines()
 
-with io.open("Readme.md", encoding="utf-8") as f:
+with io.open(os.path.join(here, "Readme.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # Handle version number with optional .dev postfix when building a develop branch
 # on AppVeyor.
-VERSION = io.open("motmetrics/__init__.py").readlines()[-1].split()[-1].strip('"')
+VERSION = io.open(os.path.join(here, "motmetrics/__init__.py")).readlines()[-1].split()[-1].strip('"')
 BUILD_NUMBER = os.environ.get("APPVEYOR_BUILD_NUMBER", None)
 BRANCH_NAME = os.environ.get("APPVEYOR_REPO_BRANCH", "develop")
 if BUILD_NUMBER is not None and BRANCH_NAME != "master":
